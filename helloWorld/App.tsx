b5b9908624f0684a918a11bcdb1ab12589455e40
Joao Marcelo Frezzato Oliveira <jmarcelofo@gmail.com>
2019-01-07 09:33:10 -0200

    Logica de validacao do login

--- helloWorld/App.tsx
+++ helloWorld/App.tsx
@@ -9,22 +9,15 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Button} from 'react-native';
-import LoginComponent from './components/LoginComponent';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, View} from 'react-native';
+import Login from './components/Login';
 
 interface Props {}
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <LoginComponent/>
+        <Login/>
       </View>
     );
   }
